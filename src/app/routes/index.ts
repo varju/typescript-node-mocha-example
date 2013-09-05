4ed3427d7ce8b7c2086be1282ed2311e3e0044a7
Alex Varju <alex@varju.ca>
2013-09-05 16:37:25 -0700

    remove unused import

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,7 +1,5 @@
 /// <reference path='../../../app.d.ts' />
 
-import express = require("express")
-
 export function index(req:ExpressServerRequest, res:ExpressServerResponse) {
   res.send({ hello: 'world' });
 }
